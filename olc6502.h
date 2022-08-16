c3d69b931ad1fd3ab380cc827e18bf40543424b3
Jens Fischer <JensFZ@gmail.com>
2022-08-16 22:49:32 +0200

    Adressing Modes hinzugeügt

--- olc6502.h
+++ olc6502.h
@@ -30,9 +30,19 @@ public:
 	uint16_t pc = 0x0000;	// Program Counter
 	uint8_t status = 0x00;	// Status
 
+
+	// Bus verbinden
 	void ConnectBus(Bus* n) {
 		bus = n;
 	}
+
+	// Addressing Modes
+	uint8_t IMP(); uint8_t IMM();
+	uint8_t ZP0(); uint8_t ZPX();
+	uint8_t ZPY(); uint8_t REL();
+	uint8_t ABS(); uint8_t ABX();
+	uint8_t ABY(); uint8_t IND();
+	uint8_t IZX(); uint8_t IZY();
 
 private:
 	Bus* bus = nullptr;
