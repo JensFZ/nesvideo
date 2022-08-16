03b0f41a92c62943deff28359bfd2f6046d074c5
Jens Fischer <JensFZ@gmail.com>
2022-08-16 22:36:52 +0200

    FLAGS enum eingebaut

--- olc6502.h
+++ olc6502.h
@@ -10,6 +10,18 @@ public:
 	olc6502();
 	~olc6502();
 
+public:
+	enum FLAGS6502 { // Bit: NVUBDIZC
+		C = (1<<0), // Carry Bit
+		Z = (1<<1), // Zero
+		I = (1<<2), // Disable Interupts
+		D = (1<<3), // Decimal Mode (wird nicht verwendet)
+		B = (1<<4), // Break
+		U = (1<<5), // Unused
+		V = (1<<6), // Overflow
+		N = (1<<7), // Negative
+	};
+
 	void ConnectBus(Bus* n) {
 		bus = n;
 	}
