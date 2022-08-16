50cba3e32034b03cee4b3530894adc2525e950c3
Jens Fischer <JensFZ@gmail.com>
2022-08-16 22:42:33 +0200

    kommentar erweitert

--- olc6502.h
+++ olc6502.h
@@ -15,7 +15,7 @@ public:
 		C = (1<<0), // Carry Bit
 		Z = (1<<1), // Zero
 		I = (1<<2), // Disable Interupts
-		D = (1<<3), // Decimal Mode (wird nicht verwendet)
+		D = (1<<3), // Decimal Mode (wird nicht verwendet vom NES)
 		B = (1<<4), // Break
 		U = (1<<5), // Unused
 		V = (1<<6), // Overflow
