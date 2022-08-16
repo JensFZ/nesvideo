64e23dca0da46d60f0f386f6914bdd91b1a839f4
Jens Fischer <JensFZ@gmail.com>
2022-08-16 22:27:36 +0200

    Projektdateien hinzufügen.

---
+++ olc6502.cpp
@@ -0,0 +1,19 @@
+#include "olc6502.h"
+
+olc6502::olc6502()
+{
+}
+
+olc6502::~olc6502()
+{
+}
+
+uint8_t olc6502::read(uint16_t a)
+{
+	return uint8_t();
+}
+
+void olc6502::write(uint16_t a, uint8_t d)
+{
+}
+
---
+++ olc6502.h
@@ -0,0 +1,22 @@
+#pragma once
+//#include "Bus.h"
+#include <cstdint>
+
+class Bus;
+
+class olc6502
+{
+public:
+	olc6502();
+	~olc6502();
+
+	void ConnectBus(Bus* n) {
+		bus = n;
+	}
+
+private:
+	Bus* bus = nullptr;
+	uint8_t read(uint16_t a);
+	void write(uint16_t a, uint8_t d);
+};
+
