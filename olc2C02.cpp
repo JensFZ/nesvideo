ef2e750d9b1d25daa322df30750d96ced974c309
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:45:06 +0200

    cpuWrite erweitert

--- olc2C02.cpp
+++ olc2C02.cpp
@@ -28,13 +28,36 @@ uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
 
 void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
 {
+    switch (addr) {
+        case 0x0000: // Control
+            break;
+        case 0x0001: // Mask
+            break;
+        case 0x0002: // Status
+            break;
+        case 0x0003: // OAM Adresse
+            break;
+        case 0x0004: // OAM Data
+            break;
+        case 0x0005: // Scroll
+            break;
+        case 0x0006: // PPU Address
+            break;
+        case 0x0007: // PPU Data
+            break;
+    }
 }
 
 uint8_t olc2C02::ppuRead(uint16_t addr, bool rdonly)
 {
-    return uint8_t();
+    uint8_t data = 0x00;
+    addr &= 0x3FFFF;
+
+    return data;
 }
 
 void olc2C02::ppuWrite(uint16_t addr, uint8_t data)
 {
+    addr &= 0x3FFF;
+
 }
