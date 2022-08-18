4b461320c0e29a20ab3d2f9c8a0255b85f753ba8
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:35:42 +0200

    ppu erweiterung

--- Bus.cpp
+++ Bus.cpp
@@ -18,6 +18,9 @@ void Bus::cpuWrite(uint16_t addr, uint8_t data)
 	if(addr >= 0x0000 && addr <= 0x1FFF) {
 		cpuRam[addr & 0x07FF] = data;
 	}
+	else if (addr >= 0x2000 && addr <= 0x3FFF) {
+		ppu.cpuWrite(addr & 0x0007, data);
+	}
 }
 
 uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly)
@@ -26,6 +29,9 @@ uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly)
 
 	if (addr >= 0x0000 && addr <= 0x1FFF) {
 		data = cpuRam[addr & 0x07FF];
+	}
+	else if (addr >= 0x2000 && addr <= 0x3FFF) {
+		data = ppu.cpuRead(addr & 0x0007);
 	}
 
 	return data;
