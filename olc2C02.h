906bad8f7972f69aaffcf904ce14d3e2b4c747f0
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:54:01 +0200

    Speicherblöcke hinzugefügt

--- olc2C02.h
+++ olc2C02.h
@@ -6,6 +6,14 @@ public:
 	olc2C02();
 	~olc2C02();
 
+
+private:
+	// Speicher:
+	uint8_t tblName[2][1024];
+	uint8_t	tblPalette[32];
+	uint8_t	tblPattern[2][4096]; // wird eigentlich nicht benötigt, da es in der Cartridge eigentlich sitzt
+
+
 	uint8_t cpuRead(uint16_t addr, bool rdonly = false);
 	void	cpuWrite(uint16_t addr, uint8_t data);
 
