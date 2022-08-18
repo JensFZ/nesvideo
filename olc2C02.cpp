f81b606eca8d2c404433b160ee4487d9602f6bf3
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:40:40 +0200

    cpuRead vorbereitet

--- olc2C02.cpp
+++ olc2C02.cpp
@@ -2,7 +2,28 @@
 
 uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
 {
-    return uint8_t();
+    uint8_t data = 0x00;
+
+    switch (addr) {
+        case 0x0000: // Control
+            break;
+        case 0x0001: // Mask
+            break;
+        case 0x0002: // Status
+            break;
+        case 0x0003: // OAM Adresse
+            break;
+        case 0x0004: // OAM Data
+            break;
+        case 0x0005: // Scroll
+            break;
+        case 0x0006: // PPU Address
+            break;
+        case 0x0007: // PPU Data
+            break;
+    }
+
+    return data;
 }
 
 void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
