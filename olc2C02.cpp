5c3598f721c90b92eddc1d7a5b66e77ef8a0fdb5
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:33:03 +0200

    cpu und ppu read und write funktionen hinzugefügt

--- olc2C02.cpp
+++ olc2C02.cpp
@@ -1,1 +1,19 @@
 #include "olc2C02.h"
+
+uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
+{
+    return uint8_t();
+}
+
+void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
+{
+}
+
+uint8_t olc2C02::ppuRead(uint16_t addr, bool rdonly)
+{
+    return uint8_t();
+}
+
+void olc2C02::ppuWrite(uint16_t addr, uint8_t data)
+{
+}
--- olc2C02.h
+++ olc2C02.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <cstdint>
 class olc2C02
 {
 public:
 	olc2C02();
 	~olc2C02();
+
+	uint8_t cpuRead(uint16_t addr, bool rdonly = false);
+	void	cpuWrite(uint16_t addr, uint8_t data);
+
+	uint8_t ppuRead(uint16_t addr, bool rdonly = false);
+	void	ppuWrite(uint16_t addr, uint8_t data);
+
 };
 
