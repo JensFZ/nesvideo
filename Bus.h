240f1f4fa0059a3d8d274feb1ef51017b096093f
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:25:22 +0200

    read und write in cpuRead und cpuWrite umbenannt

--- Bus.h
+++ Bus.h
@@ -16,7 +16,7 @@ public: //Devices auf dem Bus
 	std::array<uint8_t, 64 * 1024> ram;
 
 public: // BUS lesen und schreiben
-	void write(uint16_t addr, uint8_t data);
-	uint8_t read(uint16_t addr, bool bReadOnly = false);
+	void cpuWrite(uint16_t addr, uint8_t data);
+	uint8_t cpuRead(uint16_t addr, bool bReadOnly = false);
 };
 
