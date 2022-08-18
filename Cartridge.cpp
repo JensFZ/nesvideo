aa5d141a257f0b4c472d44cc4aa2315b19481221
Jens Fischer <JensFZ@gmail.com>
2022-08-18 23:36:28 +0200

    ppu und cpu read write eingebaut

--- Cartridge.cpp
+++ Cartridge.cpp
@@ -1,1 +1,19 @@
 #include "Cartridge.h"
+
+uint8_t Cartridge::cpuRead(uint16_t addr, bool rdonly)
+{
+    return uint8_t();
+}
+
+void Cartridge::cpuWrite(uint16_t addr, uint8_t data)
+{
+}
+
+uint8_t Cartridge::ppuRead(uint16_t addr, bool rdonly)
+{
+    return uint8_t();
+}
+
+void Cartridge::ppuWrite(uint16_t addr, uint8_t data)
+{
+}
--- Cartridge.h
+++ Cartridge.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <cstdint>
+
 class Cartridge
 {
 public: 
 	Cartridge();
 	~Cartridge();
+
+
+	uint8_t cpuRead(uint16_t addr, bool rdonly = false);
+	void	cpuWrite(uint16_t addr, uint8_t data);
+
+	uint8_t ppuRead(uint16_t addr, bool rdonly = false);
+	void	ppuWrite(uint16_t addr, uint8_t data);
+
 };
 
