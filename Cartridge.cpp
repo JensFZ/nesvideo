43c15f7f075d50b41d81f2d8031f3d3642a3cd1a
Jens Fischer <JensFZ@gmail.com>
2022-08-20 15:01:48 +0200

    constructor hinzugefügt,

--- Cartridge.cpp
+++ Cartridge.cpp
@@ -1,4 +1,24 @@
 #include "Cartridge.h"
+#include <fstream>
+
+Cartridge::Cartridge(const std::string& sFilename)
+{
+    struct sHeader {
+        char name[4];
+        uint8_t prg_rom_chunks;
+        uint8_t chr_rom_chunks;
+        uint8_t mapper1;
+        uint8_t mapper2;
+        uint8_t prg_ram_size;
+        uint8_t tv_system1;
+        uint8_t tv_system2;
+        char unused[5];
+    } header;
+
+    std::ifstream ifs;
+    ifs.open(sFilename, std::ifstream::binary);
+
+}
 
 uint8_t Cartridge::cpuRead(uint16_t addr, bool rdonly)
 {
