012f1c9d59e2927fe6382112f50e4fa4d3308056
Jens Fischer <JensFZ@gmail.com>
2022-08-20 14:55:19 +0200

    Speicherverwaltung hinzugefügt

--- Cartridge.h
+++ Cartridge.h
@@ -4,9 +4,17 @@
 class Cartridge
 {
 public: 
-	Cartridge();
+	Cartridge(const std::string& sFilename);
 	~Cartridge();
 
+
+private:
+	std::vector<uint8_t> vPRGMemory;
+	std::vector<uint8_t> vCHRMemory;
+
+	uint8_t nMapperID = 0; // Welcher Mapper wird verwendet
+	uint8_t nPRGBanks = 0; // Anzahl der Programm Bänke
+	uint8_t cCHRBanks = 0; // Anzahl der CharacterRom Bänke
 
 	uint8_t cpuRead(uint16_t addr, bool rdonly = false);
 	void	cpuWrite(uint16_t addr, uint8_t data);
